ff71835e6e65fad7e9bee6dae1b1a4551284fdbd
Junnos  <junnos.dev@gmail.com>
2024-08-29 17:09:15 +0900

    Added script file to setup Homebrew on first apply

---
+++ src/.chezmoiscripts/run_once_before_00-macos-brew.sh.tmpl.sh
@@ -0,0 +1,14 @@
+{{- if (eq .chezmoi.os "darwin") -}}
+#!/usr/bin/env bash
+set -ev pipefail
+
+# 1. Install Homebrew
+which -a brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew="$(brew --prefix)/bin/brew"
+$brew --version
+
+# 2. Install brew packages
+$brew bundle --no-lock --file={{ .chezmoi.sourceDir }}/.brewfiles/{{ .chezmoi.osid }}-Brewfile
+
+# 3. Upgrade already-installed brew packages
+$brew update && $brew upgrade
