c3dff084215bb2cc8b96e9af22d218528297939c
Junnos  <junnos.dev@gmail.com>
2024-09-01 00:43:52 +0900

    Added apt list for ubuntu

--- src/.chezmoiscripts/run_once_before_00-linux-prepare.sh.tmpl.sh
+++ src/.chezmoiscripts/run_once_before_00-linux-prepare.sh.tmpl.sh
@@ -7,6 +7,14 @@ apt update && apt upgrade -y
 apt install zsh -y
 chsh -s $(which zsh)
 
+apt-get install -y \
+    build-essential \
+    locales \
+    net-tools \
+    python3 python3-pip \
+    vim \
+    wget
+
 # 2. Install Homebrew
 which -a brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew="$(brew --prefix)/bin/brew"
