26c81fdd7dcb3a1da1f96f3965495329ca3ab2fd
Junnos  <junnos.dev@gmail.com>
2024-09-20 08:49:56 +0900

    Update to use password manager on init
    https://www.chezmoi.io/user-guide/advanced/install-your-password-manager-on-init/

---
+++ src/.install-password-manager.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    which -a brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew="$(brew --prefix)/bin/brew"
+    $brew --version
+
+    $brew install cask "1password"
+    $brew install cask "1password-cli"
+    ;;
+Linux)
+    # commands to install password-manager-binary on Linux
+    if command -v sudo &> /dev/null; then
+        SUDO="sudo"
+    else
+        SUDO=""
+    fi
+
+    if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
+        curl -sS https://downloads.1password.com/linux/keys/1password.asc | $SUDO gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    fi
+
+    if ! [ -f /etc/apt/sources.list.d/1password.list ]; then
+        echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main' | $SUDO tee /etc/apt/sources.list.d/1password.list
+    fi
+
+    if ! [ -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]; then
+        $SUDO mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+        curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | $SUDO tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+        $SUDO mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+        curl -sS https://downloads.1password.com/linux/keys/1password.asc | $SUDO gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    fi
+
+    $SUDO apt update && $SUDO apt install 1password-cli -y
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
