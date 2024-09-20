f0436c165cb3246c6a418fc82763ae558c4a71a2
Junnos  <junnos.dev@gmail.com>
2024-09-20 09:34:48 +0900

    Updated install-1password script

--- .install-1password.sh
+++ .install-1password.sh
@@ -17,28 +17,18 @@ Darwin)
     ;;
 Linux)
     # commands to install password-manager-binary on Linux
-    if command -v sudo &> /dev/null; then
-        SUDO="sudo"
-    else
-        SUDO=""
-    fi
-
-    if ! [ -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
-        curl -sS https://downloads.1password.com/linux/keys/1password.asc | $SUDO gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-    fi
-
-    if ! [ -f /etc/apt/sources.list.d/1password.list ]; then
-        echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main' | $SUDO tee /etc/apt/sources.list.d/1password.list
-    fi
-
-    if ! [ -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]; then
-        $SUDO mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-        curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | $SUDO tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-        $SUDO mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-        curl -sS https://downloads.1password.com/linux/keys/1password.asc | $SUDO gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-    fi
-
-    $SUDO apt update && $SUDO apt install 1password-cli -y
+    sudo -s \
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+    tee /etc/apt/sources.list.d/1password.list
+    mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+    tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    apt update && apt install 1password-cli -y
     ;;
 *)
     echo "unsupported OS"
