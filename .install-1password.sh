880431de9653a64857d9a76745878074287674dc
Junnos  <junnos.dev@gmail.com>
2024-09-20 10:01:34 +0900

    Update 1password installation method to manual

--- .install-1password.sh
+++ .install-1password.sh
@@ -17,18 +17,14 @@ Darwin)
     ;;
 Linux)
     # commands to install password-manager-binary on Linux
-    sudo -s \
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-    tee /etc/apt/sources.list.d/1password.list
-    mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-    tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-    mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-    gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-    apt update && apt install 1password-cli -y
+    ARCH="amd64" && \
+    wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.30.0/op_linux_${ARCH}_v2.30.0.zip" -O op.zip && \
+    unzip -d op op.zip && \
+    sudo mv op/op /usr/local/bin/ && \
+    rm -r op.zip op && \
+    sudo groupadd -f onepassword-cli && \
+    sudo chgrp onepassword-cli /usr/local/bin/op && \
+    sudo chmod g+s /usr/local/bin/op
     ;;
 *)
     echo "unsupported OS"
